7c544368ac96add81753bdafff043afcac7598d0
Jonas-Grill <jonasb.grill@gmail.com>
2021-12-10 16:02:39 +0100

    Finished initial setup

---
+++ backend/README.md
@@ -0,0 +1,8 @@
+# Overview
+## How to start app
+deno run --allow-net --allow-write --allow-read --allow-plugin --allow-env --unstable server.ts
+
+run on version 1.11.3
+
+.env datei nicht vergessen
+Bie MongoDb zu den erlaubten ip-adressen hinzufügen
--- backend/routes.ts
+++ backend/routes.ts
@@ -1,10 +1,10 @@
 import {Context, Router} from "https://deno.land/x/oak@v10.0.0/mod.ts";
-import {
-    getUserData,
-    login,
-    registration,
-} from "./controllers/userController.ts";
-import authMiddleware from "./middleware/authMiddleware.ts";
+// import {
+//     getUserData,
+//     login,
+//     registration,
+// } from "./controllers/userController.ts";
+// import authMiddleware from "./middleware/authMiddleware.ts";
 
 const router = new Router();
 
@@ -12,10 +12,10 @@ router
     .get("/", (ctx: Context) => {
         ctx.response.body = "Welcome";
     })
-    .post("/login", login)
-    .post("/registration", registration)
-    .use(authMiddleware)
-    .get("/user", getUserData)
+    // .post("/login", login)
+    // .post("/registration", registration)
+    // .use(authMiddleware)
+    // .get("/user", getUserData)
 
 router.routes();
 
