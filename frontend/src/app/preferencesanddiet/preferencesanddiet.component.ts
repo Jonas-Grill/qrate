edcb91af951f6a5ad95fd8608fa97074850060af
annekr <anne.kraemer293@gmail.com>
2022-01-06 11:33:56 +0100

    Generate Checkboxes

--- frontend/src/app/preferencesanddiet/preferencesanddiet.component.ts
+++ frontend/src/app/preferencesanddiet/preferencesanddiet.component.ts
@@ -1,26 +1,44 @@
 import {Component, OnInit} from '@angular/core';
+import {Allergene} from "./allergenes";
 
 @Component({
   selector: 'app-preferencesanddiet',
   templateUrl: './preferencesanddiet.component.html',
   styleUrls: ['./preferencesanddiet.component.scss']
 })
+
 export class PreferencesanddietComponent implements OnInit {
+  public allergene: Allergene[] = [];
+  public traces: Allergene[] = [];
+  public diets: Allergene[] = [];
 
   constructor() { }
 
   ngOnInit(): void {
+    this.allergielist.sort();
+    this.dietlist.sort();
+
+    for (let allergie of this.allergielist){
+      this.allergene.push({id: "allergie"+allergie, name:allergie, checked: false});
+      this.traces.push({id: "tracesof"+allergie, name:allergie, checked: false});
+    }
+
+    for (let diet of this.dietlist){
+      this.diets.push({id:diet, name:diet, checked: false});
+    }
   }
 
-  allergies = ["Eier","Erdnuss","Gluten","Sesam","Senf","Lupine","Sulfite","Nuss","Weichtiere","Krebstiere","Fische","Soja","Sellerie","Milch"];
-  diets = ["Vegetarisch","Pescetarisch","Vegan"];
+  allergielist = ["Eier","Erdnuss","Gluten","Sesam","Senf","Lupine","Sulfite","Nuss","Weichtiere","Krebstiere","Fische","Soja","Sellerie","Milch"];
+  dietlist = ["Vegetarisch","Pescetarisch","Vegan"];
 
   onClickNext() {
     // ROUTING
   }
 
-  onChangeAllergies() {
+  onChangeAllergies(id: string) {
     // ON CHANGE CHECKBOX IN CATEGORY ALLERGIES
+    let element = document.getElementById(id);
+
   }
 
   onChangeTracesOf() {
@@ -32,4 +50,6 @@ export class PreferencesanddietComponent implements OnInit {
   }
 
 }
+
+
 
